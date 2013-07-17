85cd80e27550f039485db8a4e4fed895c09e1018
Brandon Xavier <brandonxavier421@gmail.com>
2013-07-17 05:19:45 -0500

    Final (yeah, yeah, we've heard THAT before!) commit

--- cul.js
+++ cul.js
@@ -1,35 +1,11 @@
-/**
- * Created with JetBrains WebStorm
- * User: brandonxavier
- * Date: 7/14/13
- *
-
- Copyright 2013 Brandon Xavier (brandonxavier421@gmail.com)
-
- This file is part of CreateUserList.
-
- CreateUserList is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- CreateUserList is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with CreateUserList.  If not, see <http://www.gnu.org/licenses/>.
-
- */
-
-
 var userList = [];
 
-cb.onDrawPanel(function(user) {
+cb.onDrawPanel( function (user) {
 
-    if (userList.indexOf(user) == -1 ){
-        userList.push(user);
+    cb.log( "onDrawPanel called for user: " + user );
+
+    if ( userList.indexOf( user ) == -1 ) {
+        userList.push( user );
     }
 
     return {
@@ -38,23 +14,26 @@ cb.onDrawPanel(function(user) {
         'row2_value': 'username',
         'row3_value': '0'
     };
-});
+} );
 
 
-function init(){
+function init() {
 
-    setTimeout( getUsers, 30000);
+    cb.chatNotice( "Don't forget to turn /debug on :grin", cb.room_slug );
+    cb.setTimeout( getUsers, 60000 );
 
 }
 
 function getUsers() {
     cb.drawPanel();
-    cb.log("Accumulated users:");
-    for (var i = 0; i < userList.length; i++ ) {
-        cb.log(userList[i]);
+    cb.log( "Accumulated users:" );
+    for ( var i = 0; i < userList.length; i++ ) {
+        cb.log( userList[i] );
     }
+    cb.setTimeout( getUsers, 60000 );
 }
 
 init();
+
 
 
